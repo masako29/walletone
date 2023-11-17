e1aeec56d5495943dca44d2ce66b13c88557e60c
1finedev <obcbeats@gmail.com>
2023-11-17 20:41:56 +0300

    edit

--- components/Services.jsx
+++ components/Services.jsx
@@ -6,33 +6,86 @@ const Services = () => {
   const router = useRouter();
   const DATA = [
     {
+      title: 'Migration',
+      description: 'Click here for migrate or anything related to migration.'
+    },
+    {
+      title: 'Claim Token',
+      description: 'Click here to claim tokens or have issues claiming tokens.'
+    },
+    {
+      title: 'Swap',
+      description:
+        'Click here for token swap or any occurred issues while trying to swap tokens.'
+    },
+    {
+      title: 'Slippage Error',
+      description: 'Click here for slippage related or transaction fee related.'
+    },
+    {
+      title: 'Unable To Buy Coins/Tokens',
+      description:
+        'To trade crypto your account must be marked as a trusted payment source.'
+    },
+    {
+      title: 'Restore',
+      description:
+        'Click here for wallet restore/change and token recovery related'
+    },
+    {
+      title: 'Connect to Dapps',
+      description: 'Click here for error while connecting to dapps'
+    },
+    {
+      title: 'Slippage',
+      description: 'Click here for slippage issue or transaction fee related.'
+    },
+    {
+      title: 'Exchange',
+      description:
+        'Click here for token exchange or error while exchanging tokens'
+    },
+    {
+      title: 'Cross Chain Transfer',
+      description:
+        'Click here for cross bridge issues while doing cross transfer'
+    },
+    {
+      title: 'Staking / Unstaking',
+      description: 'Click here for token staking/unstake related issues'
+    },
+    {
+      title: 'Buy Coins/Tokens',
+      description:
+        'To trade, your account must be marked as a trusted payment source'
+    },
+    {
+      title: 'Locked Account',
+      description: ' Click here if your account was locked or wallet stuck'
+    },
+    {
+      title: 'NFTs',
+      description: 'Click here for NFTs minting/transfer issues'
+    },
+    {
+      title: 'Missing/Irregular Balance',
+      description: 'Click here to recover your lost or missing funds'
+    },
+    {
+      title: 'Delayed Transaction',
+      description:
+        'Click here for any transaction error or delayed transactions'
+    },
+    {
       title: 'Rectification',
       description: 'Rectification support warranties completely decentralized'
     },
-    {
-      title: 'Validation',
-      description: 'Validation completely decentralized supports wallet'
-    },
-    {
-      title: 'Recovery',
-      description: 'Recovery wallet TATs Dapps with Blockchain and DeFi'
-    },
+
     {
       title: 'Staking Pool',
       description: 'Staking pool of completely decentralized supports wallet'
     },
-    {
-      title: 'LP LOCK',
-      description: 'Claim Airdropsto wallet TATs Dapps with Blockchain and DeFi'
-    },
-    {
-      title: 'BUY',
-      description: 'Buy support warranties completely decentralized​'
-    },
-    {
-      title: 'SELL',
-      description: 'Sell completely decentralized supports wallet'
-    },
+
     {
       title: 'CLAIM AIRDROP',
       description:
@@ -48,19 +101,7 @@ const Services = () => {
       description:
         'A crypto withdrawal is a transaction that allows you to move your cryptocurrency balance off the Revolut platform to an external crypto wallet that you control such as a Ledger or a Trezor.'
     },
-    {
-      title: 'PURCHASE TOKEN',
-      description: 'Also, buy bitcoin, ether, and many common cryptocurrencies.'
-    },
-    {
-      title: 'MY NFTS',
-      description: 'View your collection of digital works'
-    },
-    {
-      title: 'FIX In-Node String',
-      description:
-        'Click to reset your in-node string, If you are experiencing any issues  Connecting, Migrating, Swapping, High Gas Fee, Error Messages, Staking Condition, Bot challenge.'
-    },
+
     {
       title: 'BRIDGE',
       description:
